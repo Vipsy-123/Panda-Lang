e46410e1528c7dee1f14da1dc80802acb246bb6d
vishalghige43 <vishalghige9@gmail.com>
2024-01-26 11:08:27 +0530

    Added Lexer Class

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,54 +1,17 @@
 #include"compilerLib.h"
 
-
-// vector<Token> tokanize(string str){
-//     vector<Token> tokens;
-//     string buf="";
-//     for(int i=0;i<str.length();i++){
-//         if(isspace(str.at(i)))continue;
-//         if(isalpha(str.at(i))){
-//             buf+=str.at(i);
-//             i++;
-//             while(isalnum(str.at(i))){
-//                 buf.push_back(str.at(i));
-//                 i++;
-//             }
-//             i--;
-//             if(buf=="ret")tokens.push_back({.type=TokenType::ret});
-//             else{
-//                 cout<<"you messed up!";
-//                 exit(EXIT_FAILURE);
-//             }
-//             buf.clear();
-//         }
-//         else if(isdigit(str.at(i))){
-//             buf+=str.at(i);
-//             i++;
-//             while(isdigit(str.at(i))){
-//                 buf.push_back(str.at(i));
-//                 i++;
-//             }
-//             i--;
-//             tokens.push_back({.type=TokenType::digit,.val=buf});
-//             buf.clear();
-//         }
-//         else if(str.at(i)==';'){
-//             tokens.push_back({.type=TokenType::semi});
-//         }
-//         else{
-//             cout<<"you messed up!";
-//             exit(EXIT_FAILURE);
-//         }
-//     }
-//     return tokens;
-// }
+int pandaCompiler(const string &fileContents){
     
-int main(int argc,char* argv[]){
-    string str=readFileToString(argc,argv);
-
-    Lexer lexer(str);
+    Lexer lexer(fileContents);
     vector<Token> tokens=lexer.tokenize();
     cout<<(tokens.size());
+
+    return EXIT_SUCCESS;
+}
+
+int main(int argc,char* argv[]){
     
-    return 0;
+    string fileContents=readFileToString(argc,argv);
+    return pandaCompiler(fileContents);
+
 }
