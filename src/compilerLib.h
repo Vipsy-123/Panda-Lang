0128e494983a496f26537bd3939584555ae27a6d
vishalghige43 <vishalghige9@gmail.com>
2024-02-04 14:55:08 +0530

    added Conditional Stmts

---
+++ src/compilerLib.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<fstream>
+#include<sstream>
+#include<unordered_map>
+
+using namespace std;
+
+#include"../src/0.Read Panda File/ReadFile.h"
+
+#include"../src/1.Lexer/TokenType.h"
+#include"../src/1.Lexer/Tokenizer.h"
+
+#include"../src/2.Parser/Arena.h"
+#include"../src/2.Parser/ASTNode.h"
+#include"../src/2.Parser/parser.h"
+
+#include"../src/3.CodeGenration/Genrator.h"
